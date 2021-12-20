dd98b36aa6f7654cac0595673d935e9e73997cc1
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-20 08:52:30 -0300

    home -apresenetação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 
@@ -30,7 +31,8 @@ import { HomeComponent } from './views/home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
